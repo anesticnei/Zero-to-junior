21013d714684d3ab2b95bf1f02dba7e387692014
Damian Pabian <anesticnei@gmail.com>
2023-05-08 20:35:22 +0200

    Update - Without JS - Gallery Scroll

--- f-104-egzamin/index.js
+++ f-104-egzamin/index.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.nav-container a').forEach(function(listitem){
-    listitem.addEventListener("click", () => {
-        document.body.style.overflow = "scroll";
-        const topPosition = document.getElementById(listitem.dataset.section).offsetTop;
-        window.scrollTo({
-        top: topPosition,
-        left: 0,
-        behavior: 'smooth',
+// document.querySelectorAll('.nav-container a').forEach(function(listitem){
+//     listitem.addEventListener("click", () => {
+//         document.body.style.overflow = "scroll";
+//         const topPosition = document.getElementById(listitem.dataset.section).offsetTop;
+//         window.scrollTo({
+//         top: topPosition,
+//         left: 0,
+//         behavior: 'smooth',
 
-        });
-    });
-});
+//         });
+//     });
+// });
